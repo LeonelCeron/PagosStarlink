88e472c658de79555c53d742f46e539085afa139
LeonelCeron <ceronleonel99@gmail.com>
2024-10-15 13:33:17 -0600

    ApiController & Method Settings

---
+++ backend_app/app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    //Register API (POST, formData)
+    public function register(Request $request){
+    }
+
+    //Login API (POST, formData)
+    public function login(Request $request){
+
+    }
+
+    //Refreh Token API (GET)
+    public function refreshToken(){
+
+    }
+    
+    //Logout API (GET)
+    public function logout(){
+        
+    }
+}
