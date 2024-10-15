066e7629e363fc88ffdac2bdb5b87fcda54a554b
LeonelCeron <ceronleonel99@gmail.com>
2024-10-15 14:32:46 -0600

    Register ApiController

--- backend_app/app/Http/Controllers/Api/ApiController.php
+++ backend_app/app/Http/Controllers/Api/ApiController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ApiController extends Controller
 {
     //Register API (POST, formData)
     public function register(Request $request){
+        //Data validation
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required|confirmed"
+        ]);
+
+        //Data Save
+        User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            "status" => true,
+            "message" => "User created succesfully"
+        ]);
     }
 
     //Login API (POST, formData)
